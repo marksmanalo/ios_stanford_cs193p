fc12e9d8e03b0398dde98826f41f4a6eec1200e5
Mark S Manalo <mmanalo@dealersocket.com>
2018-12-27 16:20:27 -0800

    added card flipping logic

--- Concentration/Concentration/ViewController.swift
+++ Concentration/Concentration/ViewController.swift
@@ -15,7 +15,14 @@ class ViewController: UIViewController {
     }
     
     func FlipCard(withEmoji emoji: String, on button: UIButton) {
-        
+        if button.currentTitle == emoji {
+            button.setTitle("", for: UIControl.State.normal)
+            button.backgroundColor = #colorLiteral(red: 1, green: 0.5763723254, blue: 0, alpha: 1)
+        }
+        else {
+            button.setTitle(emoji, for: UIControl.State.normal)
+            button.backgroundColor = #colorLiteral(red: 1, green: 1, blue: 1, alpha: 1)
+        }
     }
 }
 
