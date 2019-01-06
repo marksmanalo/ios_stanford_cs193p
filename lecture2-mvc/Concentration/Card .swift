8104548d6108ccfb406f32eb38f2c349905631aa
Mark S Manalo <mmanalo@dealersocket.com>
2019-01-05 20:34:39 -0800

    added main model and card model

---
+++ lecture2-mvc/Concentration/Card .swift
@@ -0,0 +1,9 @@
+//
+//  Card .swift
+//  Concentration
+//
+//  Created by Mark Manalo on 1/5/19.
+//  Copyright © 2019 DealerSocket. All rights reserved.
+//
+
+import Foundation
---
+++ lecture2-mvc/Concentration/Concentration.swift
@@ -0,0 +1,9 @@
+//
+//  Concentration.swift
+//  Concentration
+//
+//  Created by Mark Manalo on 1/5/19.
+//  Copyright © 2019 DealerSocket. All rights reserved.
+//
+
+import Foundation
